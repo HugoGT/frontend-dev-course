70e25a4c2ba932910a06c00462dbe55f449dab43
Hugo <gthugo@outlook.com>
2022-10-21 14:21:21 -0500

    Shopping cart and menus

--- js/main.js
+++ js/main.js
@@ -3,14 +3,27 @@ const navbarEmail = $('.navbar-email')
 const desktopMenu = $('.desktop-menu')
 const mobileMenu = $('.mobile-menu')
 const iconMenu = $('.icon-menu')
+const iconArrow = $('.arrow')
+const productDetail = $('.product-detail')
+const navbarShoppingCart = $('.navbar-shopping-cart')
 
 navbarEmail.addEventListener('click', toggleDesktopMenu)
+navbarShoppingCart.addEventListener('click', toggleShoppingCart)
+iconArrow.addEventListener('click', toggleShoppingCart)
 iconMenu.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu() {
+  productDetail.classList.add('inactive')
   desktopMenu.classList.toggle('inactive')
 }
 
 function toggleMobileMenu() {
+  productDetail.classList.add('inactive')
   mobileMenu.classList.toggle('inactive')
+}
+
+function toggleShoppingCart(){
+  mobileMenu.classList.add('inactive')
+  desktopMenu.classList.add('inactive')
+  productDetail.classList.toggle('inactive')
 }
